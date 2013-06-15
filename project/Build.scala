c0be4c583315454efd4daf83b50d7ecd3c29dd5d
Eike Kettner <eike.kettner@gmail.com>
2013-06-15 14:14:54 +0200

    set next development version

--- project/Build.scala
+++ project/Build.scala
@@ -66,7 +66,7 @@ object RootBuild extends Build {
   )
 
   override lazy val settings = super.settings ++ Seq(
-    version := "0.2.0",
+    version := "0.3.0-SNAPSHOT",
     organization := "org.eknet.county",
     scalaVersion := Version.scala,
     publishTo := Some("eknet-maven2" at "https://eknet.org/maven2"),
